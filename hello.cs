495fb994b38e7aaca2624a11528deaa208ee30c6
Yonghun Hwang <kimmo1028@gmail.com>
2022-02-19 17:17:48 +0900

    C# 복습 시작...

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+using static System.Console;
+
+class MainApp{
+    static void Main(String[] args)
+    {
+        WriteLine("여러분, 안녕하세요?");
+        WriteLine("반갑습니다! 복습하자");
+
+        return;
+    }
+}
